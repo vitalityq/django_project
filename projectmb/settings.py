11e2fab6de2cf05b127fe1cdc25e7f95a8a6f0f8
Vitali Clicev <myemail@mail.ru>
2021-02-15 17:11:28 +0200

    secondcom

--- projectmb/settings.py
+++ projectmb/settings.py
@@ -14,6 +14,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'core'
 ]
 
@@ -63,6 +67,15 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend'
+]
+SITE_ID = 1
 
 if ENVIRONMENT == 'production':
     DEBUG = False
