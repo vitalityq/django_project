aabb1faaf373a79801266e04142f00dbfe758d54
Vitali Clicev <myemail@mail.ru>
2021-02-16 13:54:36 +0200

    16.02.2021_3

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,9 @@ app_name = 'core'
 
 urlpatterns = [
     path('', item_list, name='item-list'),
-    path('services/', views.services, name="services")
+    path('services/', views.services, name="services"),
+    path('about-us/', views.about_us, name="about-us"),
+    path('appointment/', views.appointment, name="appointment"),
+    path('single-product/', views.single_product, name="single-product"),
+    path('single-service/', views.single_service, name="single-service")
 ]
--- core/views.py
+++ core/views.py
@@ -9,6 +9,26 @@ def item_list(request):
     return render(request, "home.html", context)
 
 
+def about_us(request):
+    context = {}
+    return render(request, "about-us.html", context)
+
+
+def appointment(request):
+    context = {}
+    return render(request, "appointment.html", context)
+
+
 def services(request):
     context = {}
     return render(request, "services.html", context)
+
+
+def single_product(request):
+    context = {}
+    return render(request, "single-product.html", context)
+
+
+def single_service(request):
+    context = {}
+    return render(request, "single-service.html", context)
